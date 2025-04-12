b6a6481c8006f97b2e968d1fa045c2a101844f2a
nandha <Nandhakrishnandev@gmail.com>
2025-04-12 20:49:11 +0530

    Changed favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import React from 'react'
 const Home = () => {
   return (
  <div className='  w-full bg-black font-space md:px-12 ' >
-<SplashCursor/>
+
     <div className=' h-[70vh]   flex items-center justify-center'>
       <div className=' max-w-4xl max-md:m-8'>
-   
-          <h1 className='text-7xl text-white max-md:text-4xl font-bold'>Fullstack Web And Mobile Application Developer</h1>
-          <p className='py-8 text-[#c7c7c7]'>I create Web and Mobile applications that not only look fantastic but also function seamlessly. Whether it's developing innovative apps, or diving into the latest web technologies, I'm all in. Let's connect and turn your ideas into digital reality!</p>
+          <h1 className='text-7xl text-white max-md:text-4xl font-bold'>Hi, I am NandhaKrishnan</h1>
+          <h1 className='text-2xl mt-2 text-white max-md:text-lg font-bold'>Fullstack Web And Mobile Application Developer</h1>
+          <p className='py-4 text-[#c7c7c7]'>I create Web and Mobile applications that not only look fantastic but also function seamlessly. Whether it's developing innovative apps, or diving into the latest web technologies, I'm all in. Let's connect and turn your ideas into digital reality!</p>
 
           <Link href='/about'> <p className=' border-2 text-white border-white w-fit px-3 py-2 rounded-full'>About Me</p></Link>
       </div>
